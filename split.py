0a639aaa35990f4a9292507692f51435874f294b
Sanjay Mane <sanjusmane@gmail.com>
2022-09-12 13:28:00 +0200

    split string in python

---
+++ split.py
@@ -0,0 +1,9 @@
+
+x = ("my name is sanjay mane")
+print(x.split())
+
+
+y = ("my name is sanjay mane, i am from kloapur")
+print(y.split(", "))
+z = ("my name is sanjay mane, i am from kolhapur, 47 yeras , civil Eng")
+print(z.split(",", 1))
